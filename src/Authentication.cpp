09815a06946a456f2efc62c45b1ce3fbdfd5a1df
Fakhri <f.sajadi@pantherx.org>
2022-01-10 12:54:57 +0330

    update tests #31

--- src/Authentication.cpp
+++ src/Authentication.cpp
@@ -5,31 +5,30 @@
 
 
 void Authentication::loginWithPassword(std::string deviceName, std::string userId, std::string password, std::string serverAddress){
-        http::client()->set_server(serverAddress);
-        mtx::identifiers::User user;
-        user = mtx::identifiers::parse<mtx::identifiers::User>(userId);
-        http::client()->login(          
-          user.localpart(),
-          password,
-          deviceName,
-          [this](const mtx::responses::Login &res, mtx::http::RequestErr err) {
-              if (err) {
-                  auto error = err->matrix_error.error;
-                  if (error.empty()){            
-                        error = err->parse_error;
-                  }
-                  std::string s = std::string (error);
-                  emit loginErrorOccurred(s);
-                  return;
-              }
-              if (res.well_known) {
-                  http::client()->set_server(res.well_known->homeserver.base_url);
-                 // nhlog::net()->info("Login requested to user server: " +
-                                     //res.well_known->homeserver.base_url);
-              }
-              //TODO put data to Database  
-              emit loginOk(res);
-          });
+http::client()->set_server(serverAddress);
+mtx::identifiers::User user;
+    user = mtx::identifiers::parse<mtx::identifiers::User>(userId);        
+    http::client()->login(          
+    user.localpart(),
+    password,
+    deviceName,
+    [this](const mtx::responses::Login &res, mtx::http::RequestErr err) {
+        if (err) {
+            auto error = err->matrix_error.error;
+            if (error.empty()){            
+                    error = err->parse_error;
+            }
+            std::string s = std::string (error);
+            emit loginErrorOccurred(s);
+            return;
+        }
+        if (res.well_known) {
+            http::client()->set_server(res.well_known->homeserver.base_url);
+            // nhlog::net()->info("Login requested to user server: " +
+                                //res.well_known->homeserver.base_url);
+        }
+        emit loginOk(res);
+    });
 
 }
 
@@ -37,7 +36,6 @@ void Authentication::loginWithPassword(std::string deviceName, std::string userI
   void Authentication::logout(){
     http::client()->logout([this](const mtx::responses::Logout &, mtx::http::RequestErr err) {
         if (err) {
-            // TODO: handle special errors
            // nhlog::net()->warn("failed to logout: {}", err);
            
             auto error = err->matrix_error.error;
@@ -47,8 +45,7 @@ void Authentication::loginWithPassword(std::string deviceName, std::string userI
             std::string s = std::string (error);            
            emit logoutErrorOccurred(s);
             return;
-        }
-        //TODO Delete account from DB via deviceId
+        }        
         emit logoutOk();
     });
   }
