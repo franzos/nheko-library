abb7de94f742a72c49c53717ca2fc06fb3a8dead
h.nasajpour <h.nasajpour@pantherx.org>
2022-02-16 12:41:56 +0330

    Handle error if recovery key was wrong and emit signal #40

--- src/Client.cpp
+++ src/Client.cpp
@@ -930,9 +930,7 @@ Client::decryptDownloadedSecrets(const std::string &recoveryKey, mtx::secret_sto
 {
     if (recoveryKey.empty())
         return;
-
     auto decryptionKey = mtx::crypto::key_from_recoverykey(recoveryKey, keyDesc);
-
     if (!decryptionKey && keyDesc.passphrase) {
         try {
             decryptionKey = mtx::crypto::key_from_passphrase(recoveryKey, keyDesc);
@@ -940,20 +938,15 @@ Client::decryptDownloadedSecrets(const std::string &recoveryKey, mtx::secret_sto
             nhlog::crypto()->error("Failed to derive secret key from passphrase: {}", e.what());
         }
     }
-
-    // if (!decryptionKey) {
-    //     QMessageBox::information(
-    //       Client::instance(),
-    //       QCoreApplication::translate("CrossSigningSecrets", "Decryption failed"),
-    //       QCoreApplication::translate("CrossSigningSecrets",
-    //                                   "Failed to decrypt secrets with the "
-    //                                   "provided recovery key or passphrase"));
-    //     return;
-    // }
+    if (!decryptionKey) {
+        auto message = "Failed to decrypt secrets with the provided recovery key or passphrase";
+        nhlog::crypto()->error(message);
+        emit _verificationManager->selfVerificationStatus()->verifyMasterKeyWithPassphraseFailed(message);
+        return;
+    }
 
     auto deviceKeys = cache::client()->userKeys(http::client()->user_id().to_string());
     mtx::requests::KeySignaturesUpload req;
-
     for (const auto &[secretName, encryptedSecret] : secrets) {
         auto decrypted = mtx::crypto::decrypt(encryptedSecret, *decryptionKey, secretName);
         if (!decrypted.empty()) {
